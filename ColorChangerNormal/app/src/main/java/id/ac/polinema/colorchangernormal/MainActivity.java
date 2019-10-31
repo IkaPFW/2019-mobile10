f70ea816a238eb6c775a8aecc820dba4e5e539ab
IkaPFW <ikapfw@gmail.com>
2019-10-31 15:35:01 +0700

    Updated task 1

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,6 +2,8 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -21,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
-		
-		rootView.setBackgroundColor(generateRandomColor());
+		final ColorViewModel cvm;
+
+		cvm = ViewModelProviders.of(this).get(ColorViewModel.class);
+		rootView.setBackgroundColor(cvm.getColor());
 
 		// Tambahkan event klik pada tombol
 		btnChangeColor.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 			public void onClick(View view) {
 				int color = generateRandomColor();
 				rootView.setBackgroundColor(color);
+				cvm.setColor(color);
 			}
 		});
 	}
